a5ccb61e1fa35dc8fa88eb381d676af4e38a9355
Haithem LIMAM <heithem.limame@gmail.com>
2022-07-24 16:25:38 +0200

    Fix redis errors

--- src/main/java/com/example/consumer/configuration/RedisConfiguration.java
+++ src/main/java/com/example/consumer/configuration/RedisConfiguration.java
@@ -1,33 +1,20 @@
 package com.example.consumer.configuration;
 
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfiguration {
-    @Value("${redis.host}")
-    private String reddisHostName;
 
-    @Value("${redis.port}")
-    private String reddisPort;
-    @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        final RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-        config.setHostName(reddisHostName);
-        config.setPort(Integer.valueOf(reddisPort));
-        return new LettuceConnectionFactory(config);
-    }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory());
+        template.setConnectionFactory(connectionFactory);
         template.setHashValueSerializer(new StringRedisSerializer());
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
